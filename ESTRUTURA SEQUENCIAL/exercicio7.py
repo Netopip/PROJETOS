247a8432adadcc23621397de78ce925a311035a8
Neto <fernandesneto1999@gmail.com>
2024-08-29 23:04:34 -0300

    Create exercicio7.py

---
+++ ESTRUTURA SEQUENCIAL/exercicio7.py
@@ -0,0 +1,5 @@
+lado = float(input('Digite o valor do lado do quadrado: '))
+
+area = lado ** 2
+dobro = area * 2
+print(f'A área do quadrado de lado {lado} é de {area}, e o dobro dessa área é {dobro}.')
