57806fc6edca177cf87165a2d004aabbbf19b735
Neto <fernandesneto1999@gmail.com>
2024-08-29 23:04:15 -0300

    Create exercicio10.py

---
+++ ESTRUTURA SEQUENCIAL/exercicio10.py
@@ -0,0 +1,12 @@
+def temperatura(temperatura):
+    f = (temperatura * 9/5) + 32
+    return f
+
+def main():
+    graus = float(input('Digite a temperatura em graus ºC: '))
+
+    trans = temperatura(graus)
+    print(f'A temperatura em ºC {graus} em Fharenheit é de {trans}ºF.')
+
+if __name__ == '__main__':
+    main()
