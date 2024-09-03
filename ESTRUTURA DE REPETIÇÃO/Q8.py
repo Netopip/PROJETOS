c215b2f2121f1c29091ec2427747e634f2831a87
Neto <fernandesneto1999@gmail.com>
2024-09-03 14:46:27 -0300

    Update Q8.py

--- ESTRUTURA DE REPETIÇÃO/Q8.py
+++ ESTRUTURA DE REPETIÇÃO/Q8.py
@@ -1,17 +1,18 @@
-def soma(lista):
-    maior = max(lista)
-    return maior
+def soma_media(lista):
+    soma = sum(lista)
+    media = sum(lista)/len(lista)
+    return soma, media
 
 
 def main():
     lista = list()
     for i in range(5):
-        numero = int(input())
+        numero = int(input('Digite um número: '))
         lista.append(numero)
-    ordem = sorted(lista)
-    print(ordem)
-    msg = soma(lista)
-    print(msg)
+        
+        
+    soma, media = soma_media(lista)
+    print(f'A soma dos numeros é {soma} e a média é {media:.2f}. ')
 
 if __name__ == '__main__':
     main()
