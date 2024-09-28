d1b3f8e9e867630a545d58711ac921eaeb18a4c2
Neto <fernandesneto1999@gmail.com>
2024-09-28 19:18:06 -0300

    Update exercicio9.py

--- ESTRUTURA SEQUENCIAL/exercicio9.py
+++ ESTRUTURA SEQUENCIAL/exercicio9.py
@@ -1,3 +1,7 @@
+'''Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+C = 5 * ((F-32) / 9).'''
+
+
 def transformar_temp(temp):
     c = 5 * ((temp- 32)/ 9)
     return c
