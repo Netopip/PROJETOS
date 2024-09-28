81a98eda21cd2b0fc7ba7c96925281bf5cf2113a
Neto <fernandesneto1999@gmail.com>
2024-09-28 19:18:12 -0300

    Update exercicio6.py

--- ESTRUTURA SEQUENCIAL/exercicio6.py
+++ ESTRUTURA SEQUENCIAL/exercicio6.py
@@ -1,3 +1,14 @@
-thislist = ["apple", "banana", "cherry"]
-thislist[1:2] = ["blackcurrant", "watermelon"]
-print(thislist)
+'''Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.'''
+from math import pi
+
+def calcular_area_circulo(raio):
+    area = pi * (raio ** 2)
+    print(f'A área dep círculo de raio {raio} é {area:.2f}cm²')
+def main():
+    
+    raio = float(input('Digite o tamanho do raio:(em cm)\n'))
+    
+    calcular_area_circulo(raio)
+    
+if __name__ == '__main__':
+    main()
