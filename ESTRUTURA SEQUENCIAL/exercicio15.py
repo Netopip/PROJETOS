12564623d30b2efaabdc416183a4265f2e836968
Neto <fernandesneto1999@gmail.com>
2024-09-28 19:18:40 -0300

    Update exercicio15.py

--- ESTRUTURA SEQUENCIAL/exercicio15.py
+++ ESTRUTURA SEQUENCIAL/exercicio15.py
@@ -1,5 +1,20 @@
-dinheiro = float(input())
-horas = int(input())
+'''Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+salário bruto.
+quanto pagou ao INSS.
+quanto pagou ao sindicato.
+o salário líquido.
+calcule os descontos e o salário líquido, conforme a tabela abaixo:
++ Salário Bruto : R$
+- IR (11%) : R$
+- INSS (8%) : R$
+- Sindicato ( 5%) : R$
+= Salário Liquido : R$
+Obs.: Salário Bruto - Descontos = Salário Líquido.
+'''
+
+
+dinheiro = float(input('Quanto voce ganha por hora:\n'))
+horas = int(input('Quantas horas trabalhadas no mês:\n'))
 salario_bruto = dinheiro * horas
 ir = salario_bruto * (11/100)
 inss = salario_bruto * (8/100)
