870140bcb54f09e328cdf2a2008103d025af92f8
Neto <fernandesneto1999@gmail.com>
2024-09-28 19:18:01 -0300

    Update exercicio8.py

--- ESTRUTURA SEQUENCIAL/exercicio8.py
+++ ESTRUTURA SEQUENCIAL/exercicio8.py
@@ -1,3 +1,5 @@
+'''Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.'''
+
 valor_hora = float(input('Qual o valor da sua hora de trabalho:'))
 horas_dia= float(input('Quantas horas voce trabalho no dia: '))
 dias_trabalhados = int(input('Quantos dias você trabalhou este mês: '))
