1ea4dc782a9811f8d5522b77f3b8e3e2720e208e
Neto <fernandesneto1999@gmail.com>
2024-09-28 19:18:15 -0300

    Update exercicio4.py

--- ESTRUTURA SEQUENCIAL/exercicio4.py
+++ ESTRUTURA SEQUENCIAL/exercicio4.py
@@ -1,3 +1,5 @@
+'''Faça um Programa que peça as 4 notas bimestrais e mostre a média.'''
+
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 nota3 = float(input('Digite a terceira nota: '))
