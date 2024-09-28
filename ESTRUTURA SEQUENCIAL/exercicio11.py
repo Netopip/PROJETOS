a7b3ead93b9beae2441e4fe5ca99bfe47afafe1f
Neto <fernandesneto1999@gmail.com>
2024-09-28 19:18:29 -0300

    Update exercicio11.py

--- ESTRUTURA SEQUENCIAL/exercicio11.py
+++ ESTRUTURA SEQUENCIAL/exercicio11.py
@@ -1,3 +1,8 @@
+'''Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+o produto do dobro do primeiro com metade do segundo .
+a soma do triplo do primeiro com o terceiro.
+o terceiro elevado ao cubo.'''
+
 def calcular_numeros(a, b, c):
     produto = (a * 2 ) * (b/2)
     soma = (a * 3) + (c)
